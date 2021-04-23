da6fde7460227c579c84ab6ad1976bfcf5224460
dliang2000 <q8liang@uwaterloo.ca>
2021-04-23 19:23:59 -0400

    add RootDriver, remove Driver

--- Benchmarks/microbenchmark/src/test/java/ca/liang/RootDriver.java
+++ Benchmarks/microbenchmark/src/test/java/ca/liang/RootDriver.java
@@ -1,14 +1,16 @@
 package ca.liang;
 
-public class Driver {
+public class RootDriver {
 	public static void main(String[] argv) {
 		ca.liang.PayRollArrayMockTest class1 = new ca.liang.PayRollArrayMockTest();
+		class1.init();
 		class1.testNoEmployees();
 		class1.testEmployeesPaidIntra();
 		class1.testSingleEmployee();
 		class1.testEmployeeIsPaid();
 		class1.testAllEmployeesArePaidArray();
 		ca.liang.PayRollMockTest class2 = new ca.liang.PayRollMockTest();
+		class2.init();
 		class2.testNoEmployees();
 		class2.testNoEmployeesIntra();
 		class2.testSingleEmployee();
@@ -17,17 +19,20 @@ public class Driver {
 		class2.testAllEmployeesArePaid2();
 		class2.testInteractionOrder();
 		ca.liang.PayRollTest class3 = new ca.liang.PayRollTest();
+		class3.init();
 		class3.testNoEmployees();
 		class3.testNoEmployeesIntra();
 		class3.testSingleEmployee();
 		class3.testEmployeeIsPaid();
 		class3.testAllEmployeesArePaid();
 		ca.liang.PayRollAnnotationMockTest class4 = new ca.liang.PayRollAnnotationMockTest();
+		class4.init();
 		class4.testNoEmployees();
 		class4.testEmployeesPaidIntra();
 		class4.testSingleEmployee();
 		class4.testEmployeeIsPaid();
 		ca.liang.PayRollArrayTest class5 = new ca.liang.PayRollArrayTest();
+		class5.init();
 		class5.testNoEmployees();
 		class5.testEmployeesPaidIntra();
 		class5.testSingleEmployee();
