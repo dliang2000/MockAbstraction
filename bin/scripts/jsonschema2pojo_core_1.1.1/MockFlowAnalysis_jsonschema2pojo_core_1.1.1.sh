cb348516e7a041828228cf4242aadba6d7cc4bd1
dliang2000 <q8liang@uwaterloo.ca>
2021-05-16 19:53:10 -0400

    add scripts for bootique and jsonschema2pojo, and update driver classes for jsonschema2pojo

---
+++ bin/scripts/jsonschema2pojo_core_1.1.1/MockFlowAnalysis_jsonschema2pojo_core_1.1.1.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#under msp everything should be identical on all computers
+source ./config.sh
+echo $MACHINE_SPECIFIC_PATH
+
+PACKAGE="org.jsonschema2pojo"
+SOOT_JAR="$MACHINE_SPECIFIC_PATH/soot_jar/sootclasses-trunk-jar-with-dependencies.jar"
+JAVA_PATH="$MACHINE_SPECIFIC_PATH/target/classes":$JCOMMANDER_JAR
+CC_CLASS="ca.uwaterloo.liang.MockAnalysisMain"
+BENCHMARK_PATH="$MACHINE_SPECIFIC_PATH/Benchmarks/jsonschema2pojo-1.1.1"
+TARGET_PATH="jsonschema2pojo-core/target/classes"
+TARGET_TEST_PATH="jsonschema2pojo-core/target/test-classes"
+BENCHMARK="jsonschema2pojo_core_1.1.1"
+DRIVER_PATH="org.jsonschema2pojo.RootDriver"
+OUTPUT_PATH="$MACHINE_SPECIFIC_PATH/analysis_output/MockAnalysis"
+
+MVN_DEPENDENCY_PATH="$BENCHMARK_PATH/jsonschema2pojo-core/mvn_dependencies"
+
+# add all the jar files needed for set_soot_classpath
+for jar in $MVN_DEPENDENCY_PATH/*; do
+  JAR_PATH=$JAR_PATH:$jar
+done
+echo $JAR_PATH
+
+cd $BENCHMARK_PATH
+
+# touch is_maven in the benchmark directory to indicate that a benchmark is mvn
+if [ -a is_maven ]; then
+  echo "it is a maven project"
+  mvn clean test
+fi
+
+java -cp $SOOT_JAR:$JAVA_PATH $CC_CLASS --benchmark $BENCHMARK --output $OUTPUT_PATH --driver $DRIVER_PATH --target $BENCHMARK_PATH/$TARGET_PATH --target-tests $BENCHMARK_PATH/$TARGET_TEST_PATH --mvn-dependencies $jars`cat benchmark_class_path`:$JAR_PATH -v
+#rm -rf "sootOutput/"
