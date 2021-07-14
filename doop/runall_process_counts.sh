76b0376a98ee2d6ca7b2f04e983053476d3f7b08
dliang2000 <q8liang@uwaterloo.ca>
2021-07-13 22:40:02 -0400

    mock counts script update

--- doop/runall_process_counts.sh
+++ doop/runall_process_counts.sh
@@ -1,59 +1,42 @@
 #!/bin/bash
 
 # bootique
+for base_analysis in basic-only context-insensitive context-insensitive-plusplus 1-object-sensitive; do
+  for n in NO_INTERPROC NORMAL; do
+    # bootique
+    echo bootique-$base_analysis-$n
+    ./process-count.py --file bootique-results/$base_analysis-counts-$n --package io.bootique
 
-for n in NO_INTERPROC NORMAL; do
-  # bootique
-  echo bootique-basic-$n
-  ./process-count.py --file bootique-results/basic-new-counts-$n --package io.bootique
-  echo bootique-CI-$n
-  ./process-count.py --file bootique-results/new-counts-$n --package io.bootique
+    # commons-collections
+    echo commons-collections-$base_analysis-$n
+    ./process-count.py --file commons-collection4-results/$base_analysis-counts-$n --package org.apache.commons.collections4
 
-  # commons-collections
-  echo commons-basic-$n
-  ./process-count.py --file commons-collection4-results/basic-new-counts-$n --package org.apache.commons.collections4
-  echo commons-CI-$n
-  ./process-count.py --file commons-collection4-results/new-counts-$n --package org.apache.commons.collections4
+    # flink-core
+    echo flink-core-$base_analysis-$n
+    ./process-count.py --file flink-core-results/$base_analysis-counts-$n --package org.apache.flink
 
-  # flink-core
-  echo flink-core-basic-$n
-  ./process-count.py --file flink-core-results/basic-new-counts-$n --package org.apache.flink
-  echo flink-core-CI-$n
-  ./process-count.py --file flink-core-results/new-counts-$n --package org.apache.flink
+    # jsonschema2pojo-core
+    echo jsonschema2pojo-$base_analysis-$n
+    ./process-count.py --file jsonschema2pojo-core-results/$base_analysis-counts-$n --package org.jsonschema2pojo
 
-  # jsonschema2pojo-core
-  echo jsonschema2pojo-basic-$n
-  ./process-count.py --file jsonschema2pojo-core-results/basic-new-counts-$n --package org.jsonschema2pojo
-  echo jsonschema2pojo-CI-$n
-  ./process-count.py --file jsonschema2pojo-core-results/new-counts-$n --package org.jsonschema2pojo
+    # maven -core
+    echo maven-core-$base_analysis-$n
+    ./process-count.py --file maven-core-results/$base_analysis-counts-$n --package org.apache.maven
 
-  # maven -core
-  echo maven-core-basic-$n
-  ./process-count.py --file maven-core-results/basic-new-counts-$n --package org.apache.maven
-  echo maven-core-CI-$n
-  ./process-count.py --file maven-core-results/new-counts-$n --package org.apache.maven
+    # microbenchmark
+    echo microbenchmark-$base_analysis-$n
+    ./process-count.py --file microbenchmark-results/$base_analysis-counts-$n --package ca.liang
 
-  # microbenchmark
-  echo microbenchmark-basic-$n
-  ./process-count.py --file microbenchmark-results/basic-new-counts-$n --package ca.liang
-  echo microbenchmark-CI-$n
-  ./process-count.py --file microbenchmark-results/new-counts-$n --package ca.liang
+    # quartz-core
+    echo quartz-core-$base_analysis-$n
+    ./process-count.py --file quartz-core-results/$base_analysis-counts-$n --package org.quartz
 
-  # quartz-core
-  echo quartz-core-basic-$n
-  ./process-count.py --file quartz-core-results/basic-new-counts-$n --package org.quartz
-  echo quartz-core-CI-$n
-  ./process-count.py --file quartz-core-results/new-counts-$n --package org.quartz
+    # vraptor-core
+    echo vraptor-core-$base_analysis-$n
+    ./process-count.py --file vraptor-core-results/$base_analysis-counts-$n --package br.com.caelum.vraptor
 
-  # vraptor-core
-  echo vraptor-core-basic-$n
-  ./process-count.py --file vraptor-core-results/basic-new-counts-$n --package br.com.caelum.vraptor
-  echo vraptor-core-CI-$n
-  ./process-count.py --file vraptor-core-results/new-counts-$n --package br.com.caelum.vraptor
-
-  # mybatis
-  # echo mybatis-basic-$n
-  # ./process-count.py --file mybatis-results/basic-new-counts-$n --package org.apache.ibatis
-  #echo mybatis-CI-$n
-  #./process-count.py --file mybatis-results/new-counts-$n --package org.apache.ibatis
+    # mybatis
+    echo mybatis-$base_analysis-$n
+    ./process-count.py --file mybatis-results/$base_analysis-counts-$n --package org.apache.ibatis
+  done
 done
