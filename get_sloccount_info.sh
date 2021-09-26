eb3897185fe5cff6f6c4ce47fed2560fd81423ad
dliang2000 <q8liang@uwaterloo.ca>
2021-09-26 19:21:57 -0400

    relocate focal method scripts for better looking, need to move to root dir to run

--- get_sloccount_info.sh
+++
@@ -1,9 +0,0 @@
-CSVFile="ReposWithTestCasesCloseToGhafaris.txt"
-
-while read -r repo testcase; do
-      # echo "$repo"
-      git clone --quiet --depth 1 "$repo" temp-linecount-repo
-      echo "$repo"
-      sloccount temp-linecount-repo
-      rm -rf temp-linecount-repo
-done < "$CSVFile"
