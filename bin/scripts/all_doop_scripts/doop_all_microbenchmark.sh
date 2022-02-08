daaa13fdc3a8079c198d23fe12303b600add8194
Patrick Lam <prof.lam@gmail.com>
2022-02-07 19:50:33 -0500

    cleanup scripts and amend .dl accordingly

--- bin/scripts/all_doop_scripts/doop_all_microbenchmark.sh
+++ bin/scripts/all_doop_scripts/doop_all_microbenchmark.sh
@@ -1,34 +1,40 @@
 #!/bin/bash
 
+# this script has to be invoked from the doop directory
+# be sure to first 1) copy the .dl files from MockAbstraction/doop to souffle-logic/analyses/mocks
+# and 2) run the driver-generator script (e.g. found in bin/scripts/$BENCHMARK)
+
 . ./config.sh
 BENCHMARK="microbenchmark"
-RESULT="payroll-test-0.0.1-SNAPSHOT"
+JVM=java-8
+COMPILED_JAR_PREFIX="payroll-test-0.0.1-SNAPSHOT"
 # doop runs for microbenchmark
+# for base_analysis in 1-object-sensitive ; do
 for base_analysis in basic-only context-insensitive context-insensitive-plusplus 1-object-sensitive; do
-#for base_analysis in context-insensitive ; do
-  if [[ "$base_analysis" -eq "basic-only" ]]; then
-    USE_WHAT=USE_BASIC
+  if [[ "$base_analysis" == "basic-only" ]]; then
+      USE_WHAT=USE_BASIC
   else
-    USE_WHAT=USE_CALLGRAPH
+      USE_WHAT=USE_CALLGRAPH
   fi
+  mkdir -p $MACHINE_SPECIFIC_PATH/results/$BENCHMARK-results/
+  echo ./doop -a $base_analysis -i $MACHINE_SPECIFIC_PATH/Benchmarks/$BENCHMARK/target/$COMPILED_JAR_PREFIX.jar -i $MACHINE_SPECIFIC_PATH/Benchmarks/$BENCHMARK/target/$COMPILED_JAR_PREFIX-tests.jar -i $MACHINE_SPECIFIC_PATH/Benchmarks/$BENCHMARK/mvn_dependencies --id $BENCHMARK-$base_analysis-NORMAL --souffle-jobs 32 --main ca.liang.RootDriver --define-cpp-macro $USE_WHAT --extra-logic souffle-logic/analyses/mocks/mocks.dl | tee $MACHINE_SPECIFIC_PATH/results/$BENCHMARK-results/$BENCHMARK-$base_analysis-NORMAL.log
+  ./doop -a $base_analysis -i $MACHINE_SPECIFIC_PATH/Benchmarks/$BENCHMARK/target/$COMPILED_JAR_PREFIX.jar -i $MACHINE_SPECIFIC_PATH/Benchmarks/$BENCHMARK/target/$COMPILED_JAR_PREFIX-tests.jar -i $MACHINE_SPECIFIC_PATH/Benchmarks/$BENCHMARK/mvn_dependencies --id $BENCHMARK-$base_analysis-NORMAL --souffle-jobs 32 --main ca.liang.RootDriver --define-cpp-macro $USE_WHAT --extra-logic souffle-logic/analyses/mocks/mocks.dl | tee $MACHINE_SPECIFIC_PATH/results/$BENCHMARK-results/$BENCHMARK-$base_analysis-NORMAL.log
 
-  echo ./doop -a $base_analysis -i $MACHINE_SPECIFIC_PATH/Benchmarks/$BENCHMARK/target/$RESULT.jar -i $MACHINE_SPECIFIC_PATH/Benchmarks/$BENCHMARK/target/$RESULT-tests.jar -i $MACHINE_SPECIFIC_PATH/Benchmarks/$BENCHMARK/mvn_dependencies --id microbenchmark-$base_analysis-NORMAL --souffle-jobs 32 --main ca.liang.RootDriver --define-cpp-macro $USE_WHAT --extra-logic souffle-logic/analyses/mocks/mocks.dl &>> $MACHINE_SPECIFIC_PATH/results/microbenchmark-results/microbenchmark-$base_analysis-NORMAL.log
-  ./doop -a $base_analysis -i $MACHINE_SPECIFIC_PATH/Benchmarks/$BENCHMARK/target/$RESULT.jar -i $MACHINE_SPECIFIC_PATH/Benchmarks/$BENCHMARK/target/$RESULT-tests.jar -i $MACHINE_SPECIFIC_PATH/Benchmarks/$BENCHMARK/mvn_dependencies --id microbenchmark-$base_analysis-NORMAL --souffle-jobs 32 --main ca.liang.RootDriver --define-cpp-macro $USE_WHAT --extra-logic souffle-logic/analyses/mocks/mocks.dl &>> $MACHINE_SPECIFIC_PATH/results/microbenchmark-results/microbenchmark-$base_analysis-NORMAL.log
-
+#  for n in NORMAL; do
   for n in NORMAL NO_INTERPROC; do
-    echo rm -rf results/$RESULT/$base_analysis/java_8/microbenchmark-$base_analysis-$n
-    rm -rf results/$RESULT/$base_analysis/java_8/microbenchmark-$base_analysis-$n
+    echo rm -rf $MACHINE_SPECIFIC_PATH/results/$base_analysis/$JVM/$BENCHMARK-$base_analysis-$n
+    rm -rf $MACHINE_SPECIFIC_PATH/results/$base_analysis/$JVM/$BENCHMARK-$base_analysis-$n
 
-    echo mkdir -p results/$RESULT/$base_analysis/java_8/microbenchmark-$base_analysis-$n
-    mkdir -p results/$RESULT/$base_analysis/java_8/microbenchmark-$base_analysis-$n
+    echo mkdir -p $MACHINE_SPECIFIC_PATH/results/$base_analysis/$JVM/$BENCHMARK-$base_analysis-$n
+    mkdir -p $MACHINE_SPECIFIC_PATH/results/$BENCHMARK-results/$base_analysis/$JVM/$BENCHMARK-$base_analysis-$n
 
-#    echo cp -a out/microbenchmark-$base_analysis-NORMAL/database/* results/$RESULT/$base_analysis/java_8/microbenchmark-$base_analysis-$n
-#    cp -a out/microbenchmark-$base_analysis-NORMAL/database/* results/$RESULT/$base_analysis/java_8/microbenchmark-$base_analysis-$n
+    echo /usr/bin/time -o $MACHINE_SPECIFIC_PATH/results/$BENCHMARK-results/souffle-time-$base_analysis-$BENCHMARK-$n souffle -F out/$BENCHMARK-$base_analysis-NORMAL/database/ -M $n souffle-logic/analyses/mocks/mocks-after.dl -D $MACHINE_SPECIFIC_PATH/results/$BENCHMARK-results/$base_analysis/$JVM/$BENCHMARK-$base_analysis-$n/
+    /usr/bin/time -o $MACHINE_SPECIFIC_PATH/results/$BENCHMARK-results/souffle-time-$base_analysis-$BENCHMARK-$n souffle -F out/$BENCHMARK-$base_analysis-NORMAL/database/ -M $n souffle-logic/analyses/mocks/mocks-after.dl -D $MACHINE_SPECIFIC_PATH/results/$BENCHMARK-results/$base_analysis/$JVM/$BENCHMARK-$base_analysis-$n/
 
-    echo /usr/bin/time -o $HOME/souffle-$base_analysis-$BENCHMARK-$n souffle -F out/microbenchmark-$base_analysis-NORMAL/database/ -M $n souffle-logic/analyses/mocks/mocks-after.dl -D results/$RESULT/$base_analysis/java_8/microbenchmark-$base_analysis-$n/
-    /usr/bin/time -o $HOME/souffle-$base_analysis-$BENCHMARK-$n souffle -F out/microbenchmark-$base_analysis-NORMAL/database/ -M $n souffle-logic/analyses/mocks/mocks-after.dl -D results/$RESULT/$base_analysis/java_8/microbenchmark-$base_analysis-$n/
+    echo cp -a results/$COMPILED_JAR_PREFIX/$n/$JVM/$BENCHMARK-$base-analysis-$n $MACHINE_SPECIFIC_PATH/results/raw/$BENCHMARK-$base-analysis-$n
+    cp -a results/$COMPILED_JAR_PREFIX/$n/$JVM/$BENCHMARK-$base-analysis-$n $MACHINE_SPECIFIC_PATH/results/raw/$BENCHMARK-$base-analysis-$n
 
-    echo ./count.py --file results/$RESULT/$base_analysis/java_8/microbenchmark-$base_analysis-$n/isMockInvocation.csv &> $HOME/results/microbenchmark-results/$base_analysis-counts-$n
-    ./count.py --file results/$RESULT/$base_analysis/java_8/microbenchmark-$base_analysis-$n/isMockInvocation.csv &> $HOME/results/microbenchmark-results/$base_analysis-counts-$n
+    echo $MACHINE_SPECIFIC_PATH/doop/count.py --file $MACHINE_SPECIFIC_PATH/results/$BENCHMARK-results/$base_analysis/$JVM/$BENCHMARK-$base_analysis-$n/isMockInvocation.csv | tee  $MACHINE_SPECIFIC_PATH/results/$BENCHMARK-results/$base_analysis-counts-$n
+    $MACHINE_SPECIFIC_PATH/doop/count.py --file $MACHINE_SPECIFIC_PATH/results/$BENCHMARK-results/$base_analysis/$JVM/$BENCHMARK-$base_analysis-$n/isMockInvocation.csv &> $MACHINE_SPECIFIC_PATH/results/$BENCHMARK-results/$base_analysis-counts-$n
   done
 done
