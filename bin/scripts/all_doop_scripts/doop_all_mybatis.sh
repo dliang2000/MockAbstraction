9e4f0921de70dc9fc1760486c9d9fe7fff706ccd
Patrick Lam <prof.lam@gmail.com>
2022-02-09 17:35:33 -0500

    mybatis scripts & results

--- bin/scripts/all_doop_scripts/doop_all_mybatis.sh
+++ bin/scripts/all_doop_scripts/doop_all_mybatis.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
 # doop runs for mybatis
+
+. ./config.sh
+export MACHINE_SPECIFIC_PATH
+export BENCHMARK="mybatis-3-mybatis-3.5.6"
+export EXTRA_BENCHMARK=""
+export JVM=java_8
+export COMPILED_JAR_PREFIX="mybatis-3.5.6"
+export MAIN_CLASS="org.apache.ibatis.RootDriver"
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+$SCRIPT_DIR/_doop_all.sh
+
+exit 0
+#!/bin/bash
+
+# doop runs for mybatis
 BENCHMARK="mybatis-3-mybatis-3.5.6"
 RESULT="mybatis-3.5.6"
 
