a42294e2b4837af0a925ea5be188f93e32cbd53a
Patrick Lam <prof.lam@gmail.com>
2022-02-09 09:55:20 -0500

    jsonschema2pojo results

--- bin/scripts/all_doop_scripts/doop_all_jsonschema.sh
+++ bin/scripts/all_doop_scripts/doop_all_jsonschema.sh
@@ -1,34 +1,12 @@
 #!/bin/bash
 
-# doop runs for commons-collections4
-BENCHMARK="jsonschema2pojo-1.1.1"
-RESULT="jsonschema2pojo-core-1.1.1"
+. ./config.sh
+export MACHINE_SPECIFIC_PATH
+export BENCHMARK="jsonschema2pojo-1.1.1"
+export EXTRA_BENCHMARK="/jsonschema2pojo-core"
+export JVM=java_8
+export COMPILED_JAR_PREFIX=jsonschema2pojo-core-1.1.1
+export MAIN_CLASS="org.jsonschema2pojo.RootDriver"
 
-# doop runs for jsonschema2pojo-1.1.1-core
-for base_analysis in basic-only context-insensitive context-insensitive-plusplus 1-object-sensitive; do
-  if [[ "$base_analysis" -eq "basic-only" ]]; then
-    USE_WHAT=USE_BASIC
-  else
-    USE_WHAT=USE_CALLGRAPH
-  fi
-
-  echo ./doop -a $base_analysis -i $HOME/Benchmarks/$BENCHMARK/jsonschema2pojo-core/target/$RESULT.jar -i $HOME/Benchmarks/$BENCHMARK/jsonschema2pojo-core/target/$RESULT-tests.jar -i $HOME/Benchmarks/$BENCHMARK/jsonschema2pojo-core/mvn_dependencies --id jsonschema2pojo-$base_analysis-NORMAL --souffle-jobs 32 --main org.jsonschema2pojo.RootDriver --define-cpp-macro $USE_WHAT --extra-logic souffle-logic/analyses/mocks/mocks.dl &>> $HOME/results/jsonschema2pojo-core-results/jsonschema2pojo-core-$base_analysis-NORMAL.log
-  ./doop -a $base_analysis -i $HOME/Benchmarks/$BENCHMARK/jsonschema2pojo-core/target/$RESULT.jar -i $HOME/Benchmarks/$BENCHMARK/jsonschema2pojo-core/target/$RESULT-tests.jar -i $HOME/Benchmarks/$BENCHMARK/jsonschema2pojo-core/mvn_dependencies --id jsonschema2pojo-$base_analysis-NORMAL --souffle-jobs 32 --main org.jsonschema2pojo.RootDriver --define-cpp-macro $USE_WHAT --extra-logic souffle-logic/analyses/mocks/mocks.dl &>> $HOME/results/jsonschema2pojo-core-results/jsonschema2pojo-core-$base_analysis-NORMAL.log
-
-  for n in NORMAL NO_INTERPROC; do
-    echo rm -rf results/$RESULT/$base_analysis/java_8/jsonschema2pojo-$base_analysis-$n
-    rm -rf results/$RESULT/$base_analysis/java_8/jsonschema2pojo-$base_analysis-$n
-
-    echo mkdir -p results/$RESULT/$base_analysis/java_8/jsonschema2pojo-$base_analysis-$n
-    mkdir -p results/$RESULT/$base_analysis/java_8/jsonschema2pojo-$base_analysis-$n
-
-#    echo cp -a out/jsonschema2pojo-$base_analysis-NORMAL/database/* results/$RESULT/$base_analysis/java_8/jsonschema2pojo-$base_analysis-$n
-#    cp -a out/jsonschema2pojo-$base_analysis-NORMAL/database/* results/$RESULT/$base_analysis/java_8/jsonschema2pojo-$base_analysis-$n
-
-    echo /usr/bin/time -o $HOME/souffle-$base_analysis-$BENCHMARK-$n souffle -F out/jsonschema2pojo-$base_analysis-NORMAL/database/ -M $n souffle-logic/analyses/mocks/mocks-after.dl -D results/$RESULT/$base_analysis/java_8/jsonschema2pojo-$base_analysis-$n/
-    /usr/bin/time -o $HOME/souffle-$base_analysis-$BENCHMARK-$n souffle -F out/jsonschema2pojo-$base_analysis-NORMAL/database/ -M $n souffle-logic/analyses/mocks/mocks-after.dl -D results/$RESULT/$base_analysis/java_8/jsonschema2pojo-$base_analysis-$n/
-
-    echo ./count.py --file results/$RESULT/$base_analysis/java_8/jsonschema2pojo-$base_analysis-$n/isMockInvocation.csv &> $HOME/results/jsonschema2pojo-core-results/$base_analysis-counts-$n
-    ./count.py --file results/$RESULT/$base_analysis/java_8/jsonschema2pojo-$base_analysis-$n/isMockInvocation.csv &> $HOME/results/jsonschema2pojo-core-results/$base_analysis-counts-$n
-  done
-done
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+$SCRIPT_DIR/_doop_all.sh
