3dee38c60618a64020fdba07080721b9136b7583
Patrick Lam <prof.lam@gmail.com>
2022-02-09 21:53:55 -0500

    results for quartz, scripts for quartz and minor change for mybatis

--- bin/scripts/all_doop_scripts/doop_all_mybatis.sh
+++ bin/scripts/all_doop_scripts/doop_all_mybatis.sh
@@ -12,38 +12,3 @@ export MAIN_CLASS="org.apache.ibatis.RootDriver"
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 $SCRIPT_DIR/_doop_all.sh
-
-exit 0
-#!/bin/bash
-
-# doop runs for mybatis
-BENCHMARK="mybatis-3-mybatis-3.5.6"
-RESULT="mybatis-3.5.6"
-
-for base_analysis in basic-only context-insensitive context-insensitive-plusplus 1-object-sensitive; do
-  if [[ "$base_analysis" -eq "basic-only" ]]; then
-    USE_WHAT=USE_BASIC
-  else
-    USE_WHAT=USE_CALLGRAPH
-  fi
-
-  echo ./doop -a $base_analysis -i $HOME/Benchmarks/$BENCHMARK/target/mybatis-3.5.6.jar -i $HOME/Benchmarks/$BENCHMARK/target/mybatis-3.5.6-tests.jar -i $HOME/Benchmarks/$BENCHMARK/mvn_dependencies --id mybatis-$base_analysis-NORMAL --souffle-jobs 32 --main org.apache.ibatis.RootDriver --define-cpp-macro $USE_WHAT --extra-logic souffle-logic/analyses/mocks/mocks.dl &>> $HOME/results/mybatis3-results/mybatis3-$base_analysis-NORMAL.log
-  ./doop -a $base_analysis -i $HOME/Benchmarks/$BENCHMARK/target/mybatis-3.5.6.jar -i $HOME/Benchmarks/$BENCHMARK/target/mybatis-3.5.6-tests.jar -i $HOME/Benchmarks/$BENCHMARK/mvn_dependencies --id mybatis-$base_analysis-NORMAL --souffle-jobs 32 --main org.apache.ibatis.RootDriver --define-cpp-macro $USE_WHAT --extra-logic souffle-logic/analyses/mocks/mocks.dl &>> $HOME/results/mybatis3-results/mybatis3-$base_analysis-NORMAL.log
-
-  for n in NORMAL NO_INTERPROC; do
-    echo rm -rf results/$RESULT/$base_analysis/java_8/mybatis-$base_analysis-$n
-    rm -rf results/$RESULT/$base_analysis/java_8/mybatis-$base_analysis-$n
-
-    echo mkdir -p results/$RESULT/$base_analysis/java_8/mybatis-$base_analysis-$n
-    mkdir -p results/$RESULT/$base_analysis/java_8/mybatis-$base_analysis-$n
-
-#    echo cp -a out/mybatis-$base_analysis-NORMAL/database/* results/$RESULT/$base_analysis/java_8/mybatis-$base_analysis-$n
-#    cp -a out/mybatis-$base_analysis-NORMAL/database/* results/$RESULT/$base_analysis/java_8/mybatis-$base_analysis-$n
-
-    echo /usr/bin/time -o $HOME/souffle-$base_analysis-$BENCHMARK-$n souffle -F out/mybatis-$base_analysis-NORMAL/database/ -M $n souffle-logic/analyses/mocks/mocks-after.dl -D results/$RESULT/$base_analysis/java_8/mybatis-$base_analysis-$n/
-    /usr/bin/time -o $HOME/souffle-$base_analysis-$BENCHMARK-$n souffle -F out/mybatis-$base_analysis-NORMAL/database/ -M $n souffle-logic/analyses/mocks/mocks-after.dl -D results/$RESULT/$base_analysis/java_8/mybatis-$base_analysis-$n/
-
-    echo ./count.py --file results/$RESULT/$base_analysis/java_8/mybatis-$base_analysis-$n/isMockInvocation.csv &> $HOME/results/mybatis3-results/$base_analysis-counts-$n
-    ./count.py --file results/$RESULT/$base_analysis/java_8/mybatis-$base_analysis-$n/isMockInvocation.csv &> $HOME/results/mybatis3-results/$base_analysis-counts-$n
-  done
-done
--- bin/scripts/all_doop_scripts/doop_all_quartz.sh
+++ bin/scripts/all_doop_scripts/doop_all_quartz.sh
@@ -1,33 +1,14 @@
 #!/bin/bash
 
 # doop runs for quartz
-BENCHMARK="quartz-quartz-2.3.1-patched"
-RESULT="quartz-core-2.3.1"
 
-for base_analysis in basic-only context-insensitive context-insensitive-plusplus 1-object-sensitive; do
-  if [[ "$base_analysis" -eq "basic-only" ]]; then
-    USE_WHAT=USE_BASIC
-  else
-    USE_WHAT=USE_CALLGRAPH
-  fi
+. ./config.sh
+export MACHINE_SPECIFIC_PATH
+export BENCHMARK="quartz-quartz-2.3.1-patched"
+export EXTRA_BENCHMARK="/quartz-core"
+export JVM=java_8
+export COMPILED_JAR_PREFIX="quartz-core-2.3.1"
+export MAIN_CLASS="org.quartz.RootDriver"
 
-  echo ./doop -a $base_analysis -i $HOME/Benchmarks/$BENCHMARK/quartz-core/target/$RESULT.jar -i $HOME/Benchmarks/$BENCHMARK/quartz-core/target/$RESULT-tests.jar -i $HOME/Benchmarks/$BENCHMARK/quartz-core/mvn_dependencies --id quartz-core-$base_analysis-NORMAL --souffle-jobs 32 --main org.quartz.RootDriver --define-cpp-macro $USE_WHAT --extra-logic souffle-logic/analyses/mocks/mocks.dl &>> $HOME/results/quartz-core-results/quartz-core-$base_analysis-NORMAL.log
-  ./doop -a $base_analysis -i $HOME/Benchmarks/$BENCHMARK/quartz-core/target/$RESULT.jar -i $HOME/Benchmarks/$BENCHMARK/quartz-core/target/$RESULT-tests.jar -i $HOME/Benchmarks/$BENCHMARK/quartz-core/mvn_dependencies --id quartz-core-$base_analysis-NORMAL --souffle-jobs 32 --main org.quartz.RootDriver --define-cpp-macro $USE_WHAT --extra-logic souffle-logic/analyses/mocks/mocks.dl &>> $HOME/results/quartz-core-results/quartz-core-$base_analysis-NORMAL.log
-
-  for n in NORMAL NO_INTERPROC; do
-    echo rm -rf results/$RESULT/$base_analysis/java_8/quartz-core-$base_analysis-$n
-    rm -rf results/$RESULT/$base_analysis/java_8/quartz-core-$base_analysis-$n
-
-    echo mkdir -p results/$RESULT/$base_analysis/java_8/quartz-core-$base_analysis-$n
-    mkdir -p results/$RESULT/$base_analysis/java_8/quartz-core-$base_analysis-$n
-
-#    echo cp -a out/quartz-core-$base_analysis-NORMAL/database/* results/$RESULT/$base_analysis/java_8/quartz-core-$base_analysis-$n
-#    cp -a out/quartz-core-$base_analysis-NORMAL/database/* results/$RESULT/$base_analysis/java_8/quartz-core-$base_analysis-$n
-
-    echo /usr/bin/time -o $HOME/souffle-$base_analysis-$BENCHMARK-$n souffle -F out/quartz-core-$base_analysis-NORMAL/database/ -M $n souffle-logic/analyses/mocks/mocks-after.dl -D results/$RESULT/$base_analysis/java_8/quartz-core-$base_analysis-$n/
-    /usr/bin/time -o $HOME/souffle-$base_analysis-$BENCHMARK-$n souffle -F out/quartz-core-$base_analysis-NORMAL/database/ -M $n souffle-logic/analyses/mocks/mocks-after.dl -D results/$RESULT/$base_analysis/java_8/quartz-core-$base_analysis-$n/
-
-    echo ./count.py --file results/$RESULT/$base_analysis/java_8/quartz-core-$base_analysis-$n/isMockInvocation.csv &> $HOME/results/quartz-core-results/$base_analysis-counts-$n
-    ./count.py --file results/$RESULT/$base_analysis/java_8/quartz-core-$base_analysis-$n/isMockInvocation.csv &> $HOME/results/quartz-core-results/$base_analysis-counts-$n
-  done
-done
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+$SCRIPT_DIR/_doop_all.sh
