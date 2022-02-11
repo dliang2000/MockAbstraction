c0627f37b19b40d7ae5fe13371a0b90b6b8da1c7
Patrick Lam <prof.lam@gmail.com>
2022-02-11 10:23:18 -0500

    regularize scripts for meta-script

---
+++ bin/scripts/prepare-benchmarks.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+if ! [ -f "config.sh" ]; then
+    echo "must create config.sh to proceed; see config.sh.template"
+fi
+
+. ./config.sh
+
+PACKED_BENCHMARKS=flink-release-1.13.0-rc1.tar.xz mybatis-3-mybatis-3.5.6.tar.xz
+ALL_BENCHMARKS=bootique-2.0.B1 commons-collections4-4.4 flink-release-1.13.0-rc1 jsonschema2pojo-1.1.1 maven-maven-3.8.1 microbenchmark mybatis-3-mybatis-3.5.6 quartz-quartz-2.3.1-patched vraptor-vraptor-parent-3.5.5
+
+(
+cd Benchmarks;
+for b in $PACKED_BENCHMARKS; do
+  tar xJvf $b
+done;
+)
+
+for b in $ALL_BENCHMARKS; do
+  cp Benchmarks/benchmark-info/$b Benchmarks/$b/benchmark_info.config
+done
+
+for b in $ALL_BENCHMARKS; do
+  if ! bin/scripts/$b/driver_generator.sh; then
+    echo "driver generator failed for $b"
+    exit 1
+  fi
+done
